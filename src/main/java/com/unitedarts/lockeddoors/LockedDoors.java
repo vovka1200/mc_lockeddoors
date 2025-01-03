8a61106a5fa04a5a6bd05880092d0ea363a661a0
Vladimir Yavorskiy <vovka@krevedko.su>
2025-01-03 22:04:26 +0300

    Mod name

--- src/main/java/com/unitedarts/lockeddoors/LockedDoors.java
+++ src/main/java/com/unitedarts/lockeddoors/LockedDoors.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.unitedarts.lockeddoors;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
@@ -30,11 +30,11 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(LockedDoors.MODID)
+public class LockedDoors
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "examplemod";
+    public static final String MODID = "lockeddoors";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
@@ -61,7 +61,7 @@ public class ExampleMod
                 output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
-    public ExampleMod()
+    public LockedDoors()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
@@ -88,7 +88,7 @@ public class ExampleMod
     private void commonSetup(final FMLCommonSetupEvent event)
     {
         // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
+        LOGGER.info("HELLO FROM LockedDoors SETUP");
 
         if (Config.logDirtBlock)
             LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
